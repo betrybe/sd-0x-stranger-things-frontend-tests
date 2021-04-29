06dd1b45f0c1a41075b72aea08330fe15af6ec75
Antônio Augusto <antonioabritto@gmail.com>
2021-04-29 11:00:13 -0300

    Update VerifyEnv.spec.js

--- cypress/integration/VerifyEnv.spec.js
+++ cypress/integration/VerifyEnv.spec.js
@@ -3,7 +3,7 @@ const REACT_APP_HAWKINS_TIMEOUT = "REACT_APP_HAWKINS_TIMEOUT";
 const REACT_APP_UPSIDEDOWN_URL = "REACT_APP_UPSIDEDOWN_URL";
 const REACT_APP_UPSIDEDOWN_TIMEOUT = "REACT_APP_UPSIDEDOWN_TIMEOUT";
 
-describe("Verifica as variáveis de ambiente do frontend", () => {
+describe("1 - Verifica se foi feito o deploy do frontend no Heroku", () => {
   it("Será validado se as variáveis do hawkins existem", () => {
     expect(Cypress.env(REACT_APP_HAWKINS_URL)).to.exist;
     expect(Cypress.env(REACT_APP_HAWKINS_TIMEOUT)).to.exist;
